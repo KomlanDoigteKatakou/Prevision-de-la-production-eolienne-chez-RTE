880c6f6070e6c1546bd1e3b7557fec5180d2ad5d
Komlan Doigté KATAKOU <doigtekatakou@gmail.com>
2025-05-13 19:49:00 +0200

    Update

--- src/02_data_preprocessing.R
+++ src/02_data_preprocessing.R
@@ -1,9 +1,13 @@
 # Agrégation des données météorologiques et de production éolienne
+
 df_prod <- df_prod0[,.(eolien = mean(eolien)), by = .(date_cible = ceiling_date(date_cible, unit = "hour"))]
 
 # Traitement de la grille géographique
+
 df_grille <- fread("data/ZC_grille_01_elargie.csv")
 dt_dpt_for_plot <- ggplot2::map_data("france") %>% filter(!stringr::str_detect(region, "Corse"))
+
+# Visualisation des zones climatiques
 
 ggplot(df_grille) + 
   geom_raster(aes(x=Lon, y=Lat, fill = factor(ZC.adapte)), alpha = 0.3, show.legend = FALSE) + 
@@ -13,7 +17,8 @@ ggplot(df_grille) +
   geom_text(aes(x=Lon, y=Lat, label = ZC.adapte), df_grille[,.(Lon= mean(Lon), Lat= mean(Lat)), by = .(ZC.adapte)]) +
   coord_quickmap()
 
-# Fusion des données météorologiques et de production éolienne pour le calcul de la puissance installée
+# Fusion des données météorologiques et de production éolienne pour la modélisation
+
 df_fr <- merge(df_meteo_zone[,.(ff100_fr = mean(ff100)), by = .(date_cible)], 
                df_prod, by = "date_cible") %>% 
   mutate(time_since_beginning = as.numeric(difftime(date_cible, ymd_hms("2017-01-01 00:00:00"))))
